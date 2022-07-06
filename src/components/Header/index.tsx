52bbc1ebe521cdd5fdcec586d05009a996cb55dc
Marcelo Rossignoli <mattosrossignoli@gmail.com>
2022-07-06 05:49:56 -0300

    entrega desafio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { MdShoppingBasket } from 'react-icons/md';
+import { MdShoppingBasket } from "react-icons/md";
+import { Link } from "react-router-dom";
 
-import logo from '../../assets/images/logo.svg';
-import { Container, Cart } from './styles';
-import { useCart } from '../../hooks/useCart';
+import logo from "../../assets/images/logo.svg";
+import { useCart } from "../../hooks/useCart";
+import { Cart, Container } from "./styles";
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  const cartSize = cart.length;
+  /* const cartSize = cart.reduce((sumAmount, product) => {
+    sumAmount += 1;
+    return sumAmount;
+  }, 0);*/
 
   return (
     <Container>
@@ -20,7 +23,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import {
-  MdDelete,
   MdAddCircleOutline,
+  MdDelete,
   MdRemoveCircleOutline,
-} from 'react-icons/md';
+} from "react-icons/md";
 
-// import { useCart } from '../../hooks/useCart';
-// import { formatPrice } from '../../util/format';
-import { Container, ProductTable, Total } from './styles';
+import { useCart } from "../../hooks/useCart";
+import { formatPrice } from "../../util/format";
+import { Container, ProductTable, Total } from "./styles";
 
 interface Product {
   id: number;
@@ -18,30 +17,33 @@ interface Product {
 }
 
 const Cart = (): JSX.Element => {
-  // const { cart, removeProduct, updateProductAmount } = useCart();
+  const { cart, removeProduct, updateProductAmount } = useCart();
 
-  // const cartFormatted = cart.map(product => ({
-  //   // TODO
-  // }))
-  // const total =
-  //   formatPrice(
-  //     cart.reduce((sumTotal, product) => {
-  //       // TODO
-  //     }, 0)
-  //   )
+  const cartFormatted = cart.map((product) => ({
+    ...product,
+    subTotal: formatPrice(product.price * product.amount),
+  }));
+  const total = formatPrice(
+    cart.reduce((sumTotal, product) => {
+      return sumTotal + product.price * product.amount;
+    }, 0)
+  );
 
   function handleProductIncrement(product: Product) {
-    // TODO
+    updateProductAmount({ productId: product.id, amount: product.amount + 1 });
   }
 
   function handleProductDecrement(product: Product) {
-    // TODO
+    updateProductAmount({
+      productId: product.id,
+      amount: product.amount - 1,
+    });
   }
 
   function handleRemoveProduct(productId: number) {
-    // TODO
+    removeProduct(productId);
   }
-
+  console.log(cartFormatted);
   return (
     <Container>
       <ProductTable>
@@ -55,52 +57,54 @@ const Cart = (): JSX.Element => {
           </tr>
         </thead>
         <tbody>
-          <tr data-testid="product">
-            <td>
-              <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-            </td>
-            <td>
-              <strong>Tênis de Caminhada Leve Confortável</strong>
-              <span>R$ 179,90</span>
-            </td>
-            <td>
-              <div>
+          {cartFormatted.map((product) => (
+            <tr data-testid="product" key={product.id}>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.price}</span>
+              </td>
+              <td>
+                <div>
+                  <button
+                    data-testid="increment-product"
+                    onClick={() => handleProductIncrement(product)}
+                    type="button"
+                  >
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input
+                    data-testid="product-amount"
+                    type="text"
+                    readOnly
+                    value={product.amount}
+                  />
+                  <button
+                    data-testid="decrement-product"
+                    onClick={() => handleProductDecrement(product)}
+                    disabled={product.amount <= 1}
+                    type="button"
+                  >
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>{product.subTotal}</strong>
+              </td>
+              <td>
                 <button
+                  data-testid="remove-product"
+                  onClick={() => handleRemoveProduct(product.id)}
                   type="button"
-                  data-testid="decrement-product"
-                // disabled={product.amount <= 1}
-                // onClick={() => handleProductDecrement()}
                 >
-                  <MdRemoveCircleOutline size={20} />
+                  <MdDelete size={20} color="#7159c1" />
                 </button>
-                <input
-                  type="text"
-                  data-testid="product-amount"
-                  readOnly
-                  value={2}
-                />
-                <button
-                  type="button"
-                  data-testid="increment-product"
-                // onClick={() => handleProductIncrement()}
-                >
-                  <MdAddCircleOutline size={20} />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$ 359,80</strong>
-            </td>
-            <td>
-              <button
-                type="button"
-                data-testid="remove-product"
-              // onClick={() => handleRemoveProduct(product.id)}
-              >
-                <MdDelete size={20} />
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
 
@@ -109,7 +113,7 @@ const Cart = (): JSX.Element => {
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$ 359,80</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
